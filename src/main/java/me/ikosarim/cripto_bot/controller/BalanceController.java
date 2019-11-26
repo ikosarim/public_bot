f2829f003075e7778fe26e4e280b744892b0044b
Rocky-po <igor.kosarim@yandex.ru>
2019-11-26 18:19:34 +0300

    * create balance controller * create balance page

---
+++ src/main/java/me/ikosarim/cripto_bot/controller/BalanceController.java
@@ -0,0 +1,9 @@
+package me.ikosarim.cripto_bot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/balance")
+public class BalanceController {
+}
