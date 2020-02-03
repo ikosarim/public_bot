c23373f049e599a5b515fed28eb8310bf6ee674f
Rocky-po <igor.kosarim@yandex.ru>
2020-02-03 17:00:52 +0300

    * add tradeHistoryServiceImpl

--- src/main/java/me/ikosarim/cripto_bot/service/TradeHistoryServiceImpl.java
+++ src/main/java/me/ikosarim/cripto_bot/service/TradeHistoryServiceImpl.java
@@ -1,15 +1,35 @@
 package me.ikosarim.cripto_bot.service;
 
+import me.ikosarim.cripto_bot.db_model.TradeHistoryEntity;
 import me.ikosarim.cripto_bot.json_model.UserTradeEntity;
+import me.ikosarim.cripto_bot.repos.TradeHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Service
 @Transactional
 public class TradeHistoryServiceImpl implements TradeHistoryService {
 
+    @Autowired
+    TradeHistoryRepository tradeHistoryRepository;
+
     @Override
     public void saveTrade(UserTradeEntity userTradeEntity) {
-
+        Timestamp timestamp = new Timestamp(userTradeEntity.getDate());
+        Date date = new Date(timestamp.getTime());
+        TradeHistoryEntity tradeHistoryEntity = TradeHistoryEntity.builder()
+                .tradeId(userTradeEntity.getTradeId())
+                .date(date)
+                .currencyPair(userTradeEntity.getPair())
+                .price(userTradeEntity.getPrice())
+                .quantity(userTradeEntity.getQuantity())
+                .amount(userTradeEntity.getAmount())
+                .tradeType(userTradeEntity.getType())
+                .build();
+        tradeHistoryRepository.save(tradeHistoryEntity);
     }
 }
